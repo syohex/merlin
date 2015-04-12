df555e1203cc5ede6f27d0cb6faa2adcaf48e2e9
Frédéric Bour <frederic.bour@lakaban.net>
2015-04-12 10:44:27 +0100

    Merlin_student: add a zipper-like trie

--- src/kernel/merlin_student.ml
+++ src/kernel/merlin_student.ml
@@ -296,3 +296,129 @@ end = struct
     let lines = List.drop_n 1 (all_lines get_item tail) in
     learn t get_parser get_item Parserhasher.empty tail lines
 end
+
+module type BRANCH = sig
+  type t
+
+  val parent: t -> t option
+  val depth: t -> int
+
+  val root: t
+  val ancestor_depth: t -> t -> int
+
+  type index
+  val index: t -> index
+  val compare: index -> index -> int
+end
+
+module type VALUE = sig
+  type t
+  val empty: t
+  val is_empty: t -> bool
+end
+
+module ZTrie (Branch : BRANCH) (V : VALUE) :
+sig
+  type t
+  val empty: t
+
+  val seek: t -> Branch.t -> t
+  val get: t -> V.t
+  val set: t -> V.t -> t
+
+  val position: t -> Branch.t
+end =
+struct
+  module Map = Map.Make(struct
+      type t = Branch.index
+      let compare = Branch.compare
+    end)
+
+  type node = {
+    children: node Map.t;
+    value: V.t;
+  }
+
+  type t = {
+    path: (node * Branch.index) list;
+    branch: Branch.t;
+    node: node;
+  }
+
+  let empty_node = {
+    children = Map.empty;
+    value = V.empty;
+  }
+  let is_empty_node n = Map.is_empty n.children && V.is_empty n.value
+
+  let get t = t.node.value
+  let set t v = {t with node = {t.node with value = v}}
+
+  let empty = { path = []; node = empty_node;
+                branch = Branch.root }
+
+  let rec move_up count node = function
+    | path when count = 0 ->
+      path, node
+    | (node',key) :: path' ->
+      let node' = {node' with children = Map.add key node node'.children} in
+      move_up (count - 1) node' path'
+    | [] -> assert false
+
+  let rec clean_up count = function
+    | path when count = 0 ->
+      path, empty_node
+    | (node',key) :: path' ->
+      let node' = {node' with children = Map.remove key node'.children} in
+      if is_empty_node node' then
+        clean_up (count - 1) path'
+      else
+        move_up (count - 1) node' path'
+    | [] -> assert false
+
+  let move_up t depth =
+    let count = Branch.depth t.branch - depth in
+    if is_empty_node t.node then
+      clean_up count t.path
+    else
+      move_up count t.node t.path
+
+  let rec unroll acc branch count =
+    if count = 0 then
+      acc
+    else match Branch.parent branch with
+      | None -> assert false
+      | Some parent ->
+        unroll (branch :: acc) parent (count - 1)
+
+  let unroll branch depth = unroll [] branch (Branch.depth branch - depth)
+
+  let rec go_down path node = function
+    | [] -> path, node, []
+    | branch :: bs ->
+      let index = Branch.index branch in
+      match Map.find index node.children with
+      | node' -> go_down ((node, index) :: path) node' bs
+      | exception Not_found ->
+        path, node, bs
+
+  let move_to t branch =
+    let depth = Branch.ancestor_depth t.branch branch in
+    let up_path, node = move_up t depth in
+    let down_path = unroll branch depth in
+    go_down up_path node down_path
+
+  let seek t branch =
+    let path, node, remaining = move_to t branch in
+    let path, node =
+      match remaining with
+      | [] -> path, node
+      | branch :: branches ->
+        let path' = List.map ~f:(fun b -> empty_node, Branch.index b) branches in
+        let path = List.rev_append path' ((node, Branch.index branch) :: path) in
+        List.tl path, empty_node
+    in
+    { path; node; branch }
+
+  let position t = t.branch
+end
